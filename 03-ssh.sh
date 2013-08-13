83c5e80baa2fb601f141734792ea2d421531245e
Philip Jägenstedt <philip@foolip.org>
2013-08-13 16:39:25 +0200

    Add /etc/ssh/sshrc to setup scripts

--- 03-ssh.sh
+++ 03-ssh.sh
@@ -10,4 +10,16 @@ EOF
 
 cat /etc/ssh/sshd_config
 
+cat > /etc/ssh/sshrc <<"EOF"
+IP="$(echo $SSH_CONNECTION | awk '{print $1}')"
+KNOWN_IPS="$HOME/.ssh/known_ips"
+if ! grep -Fqsx $IP $KNOWN_IPS; then
+  echo $IP >> $KNOWN_IPS
+  echo "$IP added to $KNOWN_IPS" | \
+    mail -s "ssh $USER@$(hostname) from $IP" philip@foolip.org
+fi
+EOF
+
+cat /etc/ssh/sshrc
+
 service ssh restart
