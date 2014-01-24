e4263a2e2ba84e0f230f71fa98653de239a55d36
Philip Jägenstedt <philip@foolip.org>
2014-01-24 03:37:57 +0000

    Open port 8777 for webvtt-webhook

--- 01-iptables.sh
+++ 01-iptables.sh
@@ -4,6 +4,7 @@ iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport ssh -j ACCEPT
 iptables -A INPUT -p tcp --dport http -j ACCEPT
 iptables -A INPUT -p tcp --dport 8731 -j ACCEPT # html-mirror
+iptables -A INPUT -p tcp --dport 8777 -j ACCEPT # webvtt-webhook
 iptables -A INPUT -p icmp -j ACCEPT
 iptables -P INPUT DROP
 
