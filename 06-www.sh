057756276038f7ff5f481ddaf1dc3804fde56e1a
Philip Jägenstedt <philip@foolip.org>
2014-12-03 20:18:31 +0100

    Tweak setup scripts for nhaed.com

--- 06-www.sh
+++ 06-www.sh
@@ -1,11 +1,9 @@
 #!/bin/sh -e
 
 apt-get install git
-git clone --recursive https://github.com/foolip/www.git /home/philip/www
+git clone --recursive https://github.com/foolip/nhaed.git /home/philip/www
 chown --recursive philip:philip /home/philip/www
 
 apt-get install nginx-light
 echo "include /home/philip/www/.nginx.conf;" > /etc/nginx/sites-available/default
 service nginx restart
-
-wget -S -O /dev/null http://localhost/ping/
